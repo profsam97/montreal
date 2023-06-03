2236ac1bbd40eb1258f305d138d859128dbceeda
profsam97 <proftoby4real@hotmail.com>
2023-06-03 20:22:01 +0100

    added coverage

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express from 'express'
+import express, {Express} from 'express'
 import cors from 'cors'
 import router from "./Routers/Router";
 import connect from "./DB/connect";
 const port : number | string=  process.env.PORT || 5000;
-const app = express();
+const app : Express = express();
 // app.use(cors({origin: true}))
 connect
 
@@ -11,8 +11,12 @@ app.use(express.json())
 
 app.use(router)
 
-app.listen(port, () => {
-    console.log('running on ' + port)
-})
+export function startServer() {
+    const server = app.listen(port, () => {
+        console.log('Running on port ' + port);
+    });
 
+    return server;
+}
+startServer()
 export default app;
